f23bb74cc0681e0e2627a46de651d3c4e463957a
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-02 15:15:49 +0200

    Review Formular mit Reactive Forms verbunden

--- src/app/add-review/add-review.ts
+++ src/app/add-review/add-review.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-review',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-review.html',
   styleUrl: './add-review.css',
 })
-export class AddReview {}
+export class AddReview {
+
+  form = new FormGroup({
+    nameControl: new FormControl<string>(''),
+    categoryControl: new FormControl<string>(''),
+    districtControl: new FormControl<string>(''),
+    ratingControl: new FormControl<string>(''),
+    commentControl: new FormControl<string>(''),
+    dateControl: new FormControl<string>(''),
+    recommendControl: new FormControl<boolean>(false),
+  });
+}
