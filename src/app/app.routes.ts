b5d9b0bb7884b57432ef0aa8992da3d68cd8b651
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-02 13:48:30 +0200

    Review hinzufügen in Navigation eingebunden

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Reviews } from './reviews/reviews';
 import { Login } from './login/login';
+import { AddReview } from './add-review/add-review';
 
 export const routes: Routes = [
     { path: '', component:Home },
     { path: 'reviews', component: Reviews },
-    { path: 'login', component: Login }
+    { path: 'login', component: Login },
+    { path: 'add-review', component: AddReview }
 ];
