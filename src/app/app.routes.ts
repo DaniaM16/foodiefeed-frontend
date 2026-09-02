a74262be4044739fe12c31466fb804b82a326991
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-02 13:28:11 +0200

    Navigation und weitere Seiten erstellt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Reviews } from './reviews/reviews';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-    { path: '', component:Home }
+    { path: '', component:Home },
+    { path: 'reviews', component: Reviews },
+    { path: 'login', component: Login }
 ];
