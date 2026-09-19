fbe8158cee4839fedbeeabecf0622e0e5159b281
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-20 01:07:37 +0200

    Sicherheitsfrage, beim löschen eines reviews

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component,HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,26 @@ import { Navbar } from "./navbar/navbar";
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
+
 export class App {
   protected readonly title = signal('foodiefeed');
+
+
+
+@HostListener('document:mousemove', ['$event'])
+mouseMove(event: MouseEvent) {
+  const sparkle = document.createElement('span');
+
+  sparkle.innerHTML = '*';
+  sparkle.className = 'sparkle';
+  sparkle.style.left = event.pageX + 'px';
+  sparkle.style.top = event.pageY + 'px';
+
+  document.body.appendChild(sparkle);
+
+  setTimeout(() => {
+    sparkle.remove();
+  }, 1000);
+}
+
 }
